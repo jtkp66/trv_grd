7e6d3419292b5d7a5780ffab4486045a8f552173
Jamie Pham <jtkp66@gmail.com>
2019-05-21 21:40:17 -0700

    Initial commit with server and user auth with jsonwebtokens

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(express.json());
+
+// Conect Database
+connectDB();
+
+app.get("/", (req, res) => res.send("API Running"));
+
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
