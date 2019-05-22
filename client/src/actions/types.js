29e8541994a5484178b87ee039650d279a8d90f9
Jamie Pham <jtkp66@gmail.com>
2019-05-21 23:40:20 -0700

    Created Register componennt and able to register users through frontend.

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -8,3 +8,4 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT = "LOGOUT";
 export const ACCOUNT_DELETED = "ACCOUNT_DELETED";
+export const CLEAR_PROFILE = "CLEAR_PROFILE";
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,35 @@
-import React, { Component } from "react";
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Register from "../components/auth/Register";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <p>App</p>
-      </div>
-    );
-  }
+// Redux
+import { Provider } from "react-redux";
+import store from "../store";
+import { loadUser } from "../actions/authActions";
+import setAuthToken from "../utils/setAuthToken";
+
+import "../App.css";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
 }
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Switch>
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
